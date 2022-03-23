44098525a27a7cf69935bee9332cf2077483c146
Matt Belesiu <matt.belesiu@gmail.com>
2022-03-23 07:56:17 -0700

    interating over iterations of iterating a

---
+++ iteration/repeat.go
@@ -0,0 +1,17 @@
+package iteration
+
+// Although I am not a fan of this hard coding of the repeated
+// value since it exsits in two differnt files, this is ok for now
+// becuase this is just for pratice iterations
+const repeatCount = 5
+
+func Repeat(character string) string {
+
+	var repeated string
+
+	for i := 0; i < repeatCount; i++ {
+		repeated += character
+	}
+
+	return repeated
+}
