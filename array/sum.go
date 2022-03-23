6edce28faed0461bdd60f93fb925ef7653ba592b
Matt Belesiu <matt.belesiu@gmail.com>
2022-03-23 08:11:10 -0700

    coolness added

--- array/sum.go
+++ array/sum.go
@@ -2,8 +2,12 @@ package sumArray
 
 func Sum(numbers [5]int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	// for i := 0; i < 5; i++ {
+	// 	sum += numbers[i]
+	// }
+
+	for _, number := range numbers {
+		sum += number
 	}
 	return sum
 }
