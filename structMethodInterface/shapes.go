b11acf371cbf7ddd8a051ae5d6f6bb36d9c2f060
Matt Belesiu <matt.belesiu@gmail.com>
2022-03-27 12:54:35 -0700

    adding : lables, removing: ambiguity

--- structMethodInterface/shapes.go
+++ structMethodInterface/shapes.go
@@ -2,10 +2,12 @@ package shapes
 
 import "math"
 
+// Shape interface
 type Shape interface {
 	Area() float64
 }
 
+// Rectangle declaration and methods
 type Rectangle struct {
 	Width  float64
 	Height float64
@@ -19,10 +21,21 @@ func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
 
+// Circle declaration and methods
 type Circle struct {
 	Radius float64
 }
 
 func (c Circle) Area() float64 {
 	return math.Pow(c.Radius, 2) * math.Pi
+}
+
+// Triangle declaration and methods
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * t.Base * t.Height
 }
