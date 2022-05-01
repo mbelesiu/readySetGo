bfd9eee75c3c3c05545fbff8dd76378057ad346c
Matt Belesiu <matt.belesiu@gmail.com>
2022-05-01 15:42:27 -0700

    adding to a dic makes mapping the dic more fun

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -3,13 +3,39 @@ package main
 import "testing"
 
 func TestSearch(t *testing.T) {
+
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	// got := Search(dictionary, "test")
-	got := dictionary.Search("test")
-	want := "this is just a test"
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+		assertStrings(t, got, want)
+	})
 
-	assertStrings(t, got, want)
+	t.Run("unknown word", func(t *testing.T) {
+		_, got := dictionary.Search("uknown")
+		assertError(t, got, ErrNotFound)
+	})
+
+}
+
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	definition := "this is just a test"
+	word := "test"
+
+	dictionary.Add(word, definition)
+
+	assertDefinition(t, dictionary, word, definition)
+
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
 
 func assertStrings(t testing.TB, got, want string) {
@@ -18,5 +44,19 @@ func assertStrings(t testing.TB, got, want string) {
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 
+	}
+}
+
+func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
+	t.Helper()
+
+	got, err := dictionary.Search(word)
+
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	if got != definition {
+		t.Errorf("got %q want %q", got, definition)
 	}
 }
