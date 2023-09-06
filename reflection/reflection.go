e10206bfe400b8b5358e2f7adc5cb11ca2eebaf4
mbelesiu <matt.belesiu@gmail.com>
2023-09-06 09:07:29 -0400

    some refactroing required

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -3,10 +3,15 @@ package reflection
 import "reflect"
 
 func walk(x interface{}, fn func(input string)) {
-	val := reflect.ValueOf(x)
-	if val.Kind() == reflect.Ptr {
-		val = val.Elem()
+	val := getValue(x)
+
+	if val.Kind() == reflect.Slice {
+		for i := 0; i < val.Len(); i++ {
+			walk(val.Index(i).Interface(), fn)
+		}
+		return
 	}
+
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
 
@@ -17,4 +22,12 @@ func walk(x interface{}, fn func(input string)) {
 			walk(field.Interface(), fn)
 		}
 	}
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+	return val
 }
